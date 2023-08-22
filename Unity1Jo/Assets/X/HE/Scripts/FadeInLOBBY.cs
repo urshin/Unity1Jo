42ff041f0a8ca3f9bc50fc4f0b8d9a3a118d2130
eun457 <eun457@users.noreply.github.com>
2023-08-22 16:34:19 +0900

    하은/0822
    LOGO/LOBBY씬 생성
    ㄴ fadeIn 구현

---
+++ Unity1Jo/Assets/X/HE/Scripts/FadeInLOBBY.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInLOBBY : MonoBehaviour
+{
+    [SerializeField] float fadeSpeed = 2.0f;
+    float alpha = 1;
+
+    [SerializeField] Image panel;
+
+    private void Awake()
+    {
+        //작업용으로 유니티 인스펙터에서 False해놔서 Awake()에서 true로 켜줌
+        panel.gameObject.SetActive(true);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    IEnumerator FadeIn()
+    {
+        yield return new WaitForSeconds(0.5f); // 초기 2초 대기
+
+        while (panel.color.a > 0)
+        {
+            alpha -= Time.deltaTime * fadeSpeed;
+            panel.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+    }
+}
