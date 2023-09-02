bc730a57af154af253fb5068f81eb166ae6310df
eun457 <eun457@users.noreply.github.com>
2023-09-02 09:00:07 +0900

    하은0902
    - ResultScene 캐릭터, 파티클 외 오브젝트들 애니메이션 구현 완료
    - 내쿠키함 구매버튼 클릭 시 상황에 따른 팝업창 구현
    ㄴ 닫기 버튼 클릭 -> 창 닫기

--- Unity1Jo/Assets/Script/FX/UIMoveScale.cs
+++ Unity1Jo/Assets/Script/FX/UIMoveScale.cs
@@ -1,26 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using DG.Tweening;
+using System.Collections;
+using UnityEngine;
 
-public class UIMoveScale : MonoBehaviour
+public class UIMoveScale : MonoBehaviour //code by.하은
 {
     [SerializeField] GameObject TitleTxt;
-    [SerializeField] float animationDuration = 0.1f;
+    [SerializeField] GameObject Point;
+    [SerializeField] GameObject Coin;
+    [SerializeField] GameObject Dialogue;
+    [SerializeField] GameObject OkBtn;
+    [SerializeField] float animationDuration = 0.1f; //커지는 애니메이션이 진행되는 시간
+    [SerializeField] float delay = 0.2f; // 각 오브젝트에 대한 딜레이 시간
+
 
     public void Start()
     {
-        MoveScaleBigger();
+        StartCoroutine(MoveScaleBigger());
     }
 
-    public void MoveScaleBigger()
+    private IEnumerator MoveScaleBigger()
     {
         if (TitleTxt != null)
         {
-            // 시작 스케일 
-            Vector3 startScale = new Vector3(1f, 1f, 1f);
+            //바꿀 스케일 
+            Vector3 changeScale = new Vector3(1f, 1f, 1f);
+            TitleTxt.GetComponent<RectTransform>().DOScale(changeScale, animationDuration);
+        }
 
-            TitleTxt.GetComponent<RectTransform>().DOScale(startScale, animationDuration);
+        yield return new WaitForSeconds(delay);
+
+        if (Point != null)
+        {
+            Vector3 changeScale = new Vector3(1f, 1f, 1f);
+            Point.GetComponent<RectTransform>().DOScale(changeScale, animationDuration);
+        }
+
+        yield return new WaitForSeconds(delay);
+
+        if (Coin != null)
+        {
+            Vector3 changeScale = new Vector3(1f, 1f, 1f);
+            Coin.GetComponent<RectTransform>().DOScale(changeScale, animationDuration);
+        }
+
+        yield return new WaitForSeconds(delay);
+
+        if (Dialogue != null)
+        {
+            Vector3 changeScale = new Vector3(1f, 1f, 1f);
+            Dialogue.GetComponent<RectTransform>().DOScale(changeScale, animationDuration);
+        }
+
+        yield return new WaitForSeconds(delay);
+
+        if (OkBtn != null)
+        {
+            Vector3 changeScale = new Vector3(1f, 1f, 1f);
+            OkBtn.GetComponent<RectTransform>().DOScale(changeScale, animationDuration);
         }
     }
 }
