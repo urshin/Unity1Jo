e48ada76557f8069f91b85f301ca1084905d64ae
eun457 <eun457@users.noreply.github.com>
2023-09-06 14:41:16 +0900

    하은
    - 캐릭터별 jump/slide/DoubleJump 사운드 추가

--- Unity1Jo/Assets/Script/Player/PlayerSlideState.cs
+++ Unity1Jo/Assets/Script/Player/PlayerSlideState.cs
@@ -5,17 +5,41 @@ using UnityEngine;
 
 public class PlayerSlideState : PlayerState
 {
+    [SerializeField] string effectAudioClipPath = "Basic_Slide";
+    [SerializeField] string effectAudioClipPath1 = "Pancake_Slide";
+    [SerializeField] string effectAudioClipPath2 = "Moonlight_Slide";
     public PlayerSlideState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
     {
     }
-
-    
 
     public override void Enter()
     {
         base.Enter();
         PlayerManager.Instance.SetOriginPlayerPositi2on();
-        
+
+        //데이터 로드
+        HE_DataManager.instance.LoadData();
+        MycookiesData data = HE_DataManager.instance.GetMycookiesDatas().Find(cookie => cookie.id == UserDataManager.Instance.GetSelectCookieID());
+
+        //Effect재생
+        if (data.id == 100)
+        {
+            AudioClip effectAudioClip = GameManager.Instance.LoadAudioClip(effectAudioClipPath);
+            if (effectAudioClip != null)
+                SoundManager.Instance.Play(effectAudioClip, Define.Sound.Effect);
+        }
+        else if (data.id == 101)
+        {
+            AudioClip effectAudioClip = GameManager.Instance.LoadAudioClip(effectAudioClipPath1);
+            if (effectAudioClip != null)
+                SoundManager.Instance.Play(effectAudioClip, Define.Sound.Effect);
+        }
+        else if (data.id == 102)
+        {
+            AudioClip effectAudioClip = GameManager.Instance.LoadAudioClip(effectAudioClipPath2);
+            if (effectAudioClip != null)
+                SoundManager.Instance.Play(effectAudioClip, Define.Sound.Effect);
+        }
 
         collider.size = new Vector2(collider.size.x * 1.15f, collider.size.y / 2.16f);
         //collider.size = new Vector2(1.5f, 0.6f);
